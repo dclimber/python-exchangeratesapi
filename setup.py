b7b4a650c43ca288aef825fa980e331b789716e1
dclimber <dastand.climber@gmail.com>
2020-03-12 23:10:49 +0600

    chore: Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-exchangeratesapi-io",
+    version="0.0.1",
+    author="Dastan Abdrakhmanov",
+    author_email="dastand.climber@gmail.com",
+    description="Simple python api wrapper for exchangeratesapi.io",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dclimber/python-exchangeratesapi-io",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
